089ed4e0f56ec4541d46c2853f792a86cb2c443d
70NY <53057036+TonyGLL@users.noreply.github.com>
2020-06-06 20:45:07 -0500

    Section 3

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+
+
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const app_routes: Routes = [
+
+  {path: 'home', component: PortfolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'item', component: ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+@NgModule({
+
+  imports: [
+
+    RouterModule.forRoot(app_routes, { useHash: true })
+  ],
+  exports: [
+
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
+import { AppRoutingModule } from './app-routing.module';
+
+// Modules
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
