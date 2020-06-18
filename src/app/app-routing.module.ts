c0d9f06508117c22c9421305bf8ad568a5d07c9e
70NY <53057036+TonyGLL@users.noreply.github.com>
2020-06-18 15:44:05 -0500

    update 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 import { SearchComponent } from './pages/search/search.component';
@@ -12,19 +10,17 @@ import { SkillsComponent } from './pages/skills/skills.component';
 
 const appRoutes: Routes = [
 
-  { path: 'home', component: PortfolioComponent },
-
-  { path: 'about', component: AboutComponent },
-
   { path: 'projects', component: ProjectsComponent },
 
   { path: 'skills', component: SkillsComponent },
+
+  { path: 'about', component: AboutComponent },
 
   { path: 'item/:id', component: ItemComponent },
 
   { path: 'search/:term', component: SearchComponent },
 
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: '**', pathMatch: 'full', redirectTo: 'projects' }
 
 ];
 
